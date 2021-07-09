18bfde94086bd0fdc374d61a98b3d0706d4662af
Marli Frost <marli@frost.red>
2021-07-09 10:07:20 +0100

    depend on exclusive features with unique crate name

--- impl-a/src/lib.rs
+++ impl-a/src/lib.rs
@@ -1,1 +1,3 @@
+use impl_a_dep_with_features as dep_with_features;
+
 pub use dep_with_features::VALUE;
--- impl-b/src/lib.rs
+++ impl-b/src/lib.rs
@@ -1,1 +1,2 @@
+use impl_b_dep_with_features as dep_with_features;
 pub use dep_with_features::VALUE;
